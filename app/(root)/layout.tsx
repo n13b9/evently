ae9e303716293a133d01f3d2fa9fb0cc138eb8db
n13b9 <neeraj.barla92@gmail.com>
2024-02-17 00:26:37 +0530

    first

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../globals.css";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+
+const poppins = Poppins({ subsets: ["latin"],weight:['400','500','700'],variable:'--font-poppins' });
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "platform for events",
+  icons:{
+    icon:'/assets/images/logo-svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+        <Header/>
+      <main className="flex-1">{children}</main>
+        <Footer/>
+    </div>
+  );
+}
